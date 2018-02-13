a3d580967e9718da389ec78182755b3a74a6d739
mas <ma99.dev@gmail.com>
2018-02-13 10:41:13 +0300

    add tables

---
+++ database/migrations/2018_02_13_071547_create_districts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDistrictsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('districts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->tinyInteger('division_id')->unsigned();
+            $table->string('name', 30);
+            $table->string('bn_name', 50);
+            $table->double('lat');
+            $table->double('lon');
+            $table->string('website', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('districts');
+    }
+}
