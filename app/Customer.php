cb95c97f52d073cf1483080e0a199b0306703220
ma99 <ma99.dev@gmail.com>
2018-02-13 20:21:04 +0300

    add tables

--- app/Customer.php
+++ app/Customer.php
@@ -9,4 +9,9 @@ class Customer extends Model
     protected $fillable = [
         'name', 'phone', 'address',
     ];
+
+    public function bookings()
+    {
+        return $this->hasMany(Booking::class);
+    }
 }
